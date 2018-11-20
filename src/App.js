adc3e7ffcbf83adb93b3672b6fc8d979e7bc475b
Maxence Bouret <maxence.bouret@gmail.com>
2018-11-20 11:01:23 +0100

    End of the iteration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import Navbar from './Navbar'
+import FormField from './FormField'
+import CoolButton from './CoolButton'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navbar />
+
+        <div className="container">
+          <form>
+            <FormField label="Name" type="text" placeholder="Alex Smith" />
+            <FormField label="Email" type="email" placeholder="alexsmith@gmail.com" />
+            <FormField label="Password" type="password" placeholder="********" />
+            <CoolButton isDanger isRounded>Submit</CoolButton>
+          </form>
+        </div>
       </div>
     );
   }
---
+++ src/CoolButton.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+
+function CoolButton(props) {
+  let dictionary = {
+    isActive: 'is-active',
+    isBlack: 'is-black',
+    isCentered: 'is-centered',
+    isDanger: 'is-danger',
+    isDark: 'is-dark',
+    isFocused: 'is-focused',
+    isGrouped: 'is-grouped',
+    isHovered: 'is-hovered',
+    isInfo: 'is-info',
+    isInverted: 'is-inverted',
+    isLarge: 'is-large',
+    isLight: 'is-light',
+    isLink: 'is-link',
+    isLoading: 'is-loading',
+    isMedium: 'is-medium',
+    isOutlined: 'is-outlined',
+    isPrimary: 'is-primary',
+    isRight: 'is-right',
+    isRounded: 'is-rounded',
+    isSelected: 'is-selected',
+    isSmall: 'is-small',
+    isStatic: 'is-static',
+    isSuccess: 'is-success',
+    isText: 'is-text',
+    isWarning: 'is-warning',
+    isWhite: 'is-white',
+  }
+
+  let className = "button "
+
+  for (let key in dictionary) {
+    console.log("key", key);
+    if (props[key]) {
+      className += dictionary[key] + " "
+    }
+  }
+
+  if (props.className) className += props.className
+
+  return (
+    <button className={className}>
+      {props.children}
+    </button>
+  );
+}
+
+export default CoolButton;
